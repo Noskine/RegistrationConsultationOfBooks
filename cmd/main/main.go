bc7801568072630e4c51eac6a0a97c3beab03649
Noskine <noskine@outlook.com>
2023-11-13 22:24:46 -0300

    O ponta pé inicial

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,9 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Iniciando um projeto muito FODA em go!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Noskine/RegistrationConsultationOfBooks
+
+go 1.21.3
